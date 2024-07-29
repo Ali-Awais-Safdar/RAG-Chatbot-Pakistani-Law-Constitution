375e663e40594c42c30a9ca47edf68eb3b4d6fd7
Zophiel Suleman <zophielsuleman27@gmail.com>
2024-07-29 21:05:05 +0500

    Implemented unit tests for tokenization

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import pytest
 
 if __name__ == "__main__":
-    pytest.main(["-k", "TestCleanLegalText or TestNormalizeLegalText"]) # added the class names to run specific tests
+    pytest.main(["-k", "TestTokenizeLegalText or TestCleanLegalText or TestNormalizeLegalText"]) # added the class names to run specific tests
