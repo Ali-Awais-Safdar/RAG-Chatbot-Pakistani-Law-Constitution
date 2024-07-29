00f93ea9a209380b12e3689af586fd9a41bd7b84
Rukhshankhan <rukhshankhan7@gmail.com>
2024-07-29 21:40:26 +0500

    updated _init_.py file

--- text_processing/__init__.py
+++ text_processing/__init__.py
@@ -18,7 +18,8 @@ Version:
 from .cleaning import clean_legal_text
 from .normalization import normalize_legal_text
 from .tokenization import tokenize_legal_text
+from .pattern_matching import extract_patterns
 
-__all__ = ['clean_legal_text', 'normalize_legal_text', 'tokenize_legal_text']
+__all__ = ['clean_legal_text', 'normalize_legal_text', 'tokenize_legal_text','extract_patterns']
 
 __version__ = '1.0.0'
