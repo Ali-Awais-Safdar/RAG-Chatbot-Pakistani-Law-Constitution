72e593ce30efb1f6ec966d5e60a3d0d3c6eaf372
Abdullah Sajid <100178313+asfm2003@users.noreply.github.com>
2024-07-29 20:20:42 +0500

    Update __init__.py

--- text_processing/__init__.py
+++ text_processing/__init__.py
@@ -1,5 +1,24 @@
+"""
+LegalTextProcessor is a package for processing legal text.
+
+Modules:
+- cleaning: Contains functions to clean legal text.
+- normalization: Contains functions to normalize legal text.
+- tokenization: Contains functions to tokenize legal text.
+
+Functions:
+- clean_legal_text: Function to clean legal text.
+- normalize_legal_text: Function to normalize legal text.
+- tokenize_legal_text: Function to tokenize legal text.
+
+Version:
+1.0.0
+"""
+
 from .cleaning import clean_legal_text
 from .normalization import normalize_legal_text
 from .tokenization import tokenize_legal_text
 
-__all__ = ['clean_legal_text','tokenize_legal_text','normalize_legal_text']
+__all__ = ['clean_legal_text', 'normalize_legal_text', 'tokenize_legal_text']
+
+__version__ = '1.0.0'
