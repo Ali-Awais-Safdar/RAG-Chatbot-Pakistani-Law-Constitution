74cf8cd4b1a7bcf7dba5f95adf604dfeff66905c
Abdullah Sajid <100178313+asfm2003@users.noreply.github.com>
2024-07-30 12:37:48 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-import pytest
+import cv2
+from my_package import preprocess_image, perform_ocr
 
-if __name__ == "__main__":
-    pytest.main(["-k", "TestTokenizeLegalText or TestCleanLegalText or TestNormalizeLegalText"]) # added the class names to run specific tests
+# Example usage
+image_path = r'C:\Users\PMLS\Desktop\RAG\sample.jpg'
+preprocessed_image = preprocess_image(image_path)
+cv2.imwrite(r'C:\Users\PMLS\Desktop\RAG\preprocessed_sample.jpg', preprocessed_image)
+
+# Perform OCR
+text = perform_ocr(preprocessed_image)
+print("OCR Result:")
+print(text)
