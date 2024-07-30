87fdc18de4c06aa67b0149e47eb1afaf95473b1b
Rukhshankhan <96606306+Rukhshankhan@users.noreply.github.com>
2024-07-30 16:20:27 +0500

    Update __init__.py

--- text_processing/__init__.py
+++ text_processing/__init__.py
@@ -5,12 +5,13 @@ Modules:
 - cleaning: Contains functions to clean legal text.
 - normalization: Contains functions to normalize legal text.
 - tokenization: Contains functions to tokenize legal text.
+- pattern matching: Contains function to extract patterns from legal text.
 
 Functions:
 - clean_legal_text: Function to clean legal text.
 - normalize_legal_text: Function to normalize legal text.
 - tokenize_legal_text: Function to tokenize legal text.
-
+- extract_patterns: Function to extract predefined legal patterns from legal text.
 Version:
 1.0.0
 """
